0a85527673eca4e73b387b0fa8b7b0edd394e39d
Mohit Sharma <starcollect01@gmail.com>
2025-06-12 11:53:15 +0530

    new

---
+++ ds_python.py
@@ -0,0 +1,8 @@
+a=10
+b=True
+c="name"
+print(type(a))
+print(type(b))
+print(type(c))
+
+#data types: int, bool, str
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+s3="Hello World"
+# print(s3.zfill(20)) # fill the string with 0s to make it 20 characters long
+# print(s3.rjust(20,"1")) # justify the string to the right with 1s
+# print(s3.ljust(20,"2")) # justify the string to the left with 2s
